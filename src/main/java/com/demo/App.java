a837fcead55581b1fe84b0c77ad3ad417a2e7632
jcdominguez <jcd.pro@gmail.com>
2023-07-21 10:07:23 +0200

    1er bean Spring

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -1,13 +1,35 @@
 package com.demo;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        // Créez un contexte d'application Spring
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+
+        // Enregistrez la classe de configuration du contexte Spring
+        context.register(AppConfig.class);
+
+        // Rafraîchissez le contexte pour initialiser les beans
+        context.refresh();
+
+        // Obtenez un bean à partir du contexte
+        HelloService helloService = context.getBean(HelloService.class);
+
+        // Appelez une méthode sur le bean
+        String message = helloService.sayHello("John");
+
+        // Affichez le message
+        System.out.println(message);
+
+        // Fermez le contexte
+        context.close();
+
     }
 }
